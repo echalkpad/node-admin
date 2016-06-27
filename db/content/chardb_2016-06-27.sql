76cdd38fd90fc93d4d65b4c235a9f0cac944e01c
Daniel Dihardja <daniel@dihardja.de>
2016-06-27 12:22:32 +0200

    data update

--- db/content/chardb_2016-06-27.sql
+++ db/content/chardb_2016-06-27.sql
@@ -7,7 +7,7 @@
 #
 # Host: 192.168.99.100 (MySQL 5.6.30-76.3)
 # Database: chardb
-# Generation Time: 2016-06-27 09:21:32 +0000
+# Generation Time: 2016-06-27 10:22:13 +0000
 # ************************************************************
 
 
@@ -40,17 +40,22 @@
 
 
 
+# Dump of table dialog_user_input
+# ------------------------------------------------------------
+
+
+
 # Dump of table mood
 # ------------------------------------------------------------
 
 LOCK TABLES `mood` WRITE;
 /*!40000 ALTER TABLE `mood` DISABLE KEYS */;
 
-INSERT INTO `mood` (`id`, `title`, `description`)
+INSERT INTO `mood` (`id`, `title`)
 VALUES
-	(1,'Neutral',NULL),
-	(2,'Amicable',NULL),
-	(3,'Provocative',NULL);
+	(1,'Neutral'),
+	(2,'Amicable'),
+	(3,'Provocative');
 
 /*!40000 ALTER TABLE `mood` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -79,14 +84,29 @@ LOCK TABLES `sentence` WRITE;
 
 INSERT INTO `sentence` (`id`, `mood_id`, `title`)
 VALUES
-	(1,1,'Oh, Hi there and welcome.'),
-	(2,1,'How are you doing over there?'),
-	(3,1,'You maybe wondering who I am and what I’m doing here, huh?'),
-	(4,1,'Well, to get things started, let me tell a little bit about myself.'),
-	(5,1,'First of all: I’m your Character.'),
-	(6,1,'Actually, I’m not only your Character, but “Character” is also the name people gave me.'),
-	(7,1,'Yeah, I know, it’s a strange one, people are curious about it all the time. For convenience sake, just call me “C” '),
-	(8,1,'But enough of me, how about you? With whom do I have the pleasure of speaking?');
+	(1,1,'Oh, Hi there and welcome'),
+	(2,1,'How are you doing today?'),
+	(3,1,'The same, thanks for asking!'),
+	(4,1,'So, with whom do I have the pleasure of speaking?'),
+	(5,1,'I see. Pretty cool name!'),
+	(6,1,'Rather unusual name, huh? I like it!'),
+	(7,1,'Nevertheless, Hi there! It’s nice to meet you.'),
+	(8,1,'You’re maybe wondering what my name is and\rwhat I’m doing here.'),
+	(9,1,'You’re maybe wondering what my name is and what I’m doing here.'),
+	(10,1,'Yeah, I get that it’s quite unusual for people to name each other during their first get-together, but nevertheless, could you help me with the namefinding?'),
+	(11,1,'Even a silly one would be great. You can’t imagine how it feels to be nameless ... '),
+	(12,1,'Aw, thank you! I like it a lot!'),
+	(13,1,'So, now that we know each other a little bit, you still may wondering why I’m here. So, let me enlighten you:'),
+	(14,1,'Hm ... when I think about it, “companion” sounds rather technical. Imagine me more like a new friend of yours, who’s accompanying you during your daily living.'),
+	(15,1,'When I’m talking about accompanying, I’m actually talking about hanging out with you, just like you’re hanging out with your friends!'),
+	(16,1,'You do?! Well, that’s even more unusual.'),
+	(17,1,'You seem more like the lazy type, aren’t you?'),
+	(18,1,'But be it as it may, maybe you can think of something later.'),
+	(19,1,'In the meantime, let me tell you what it is, I’m actually doing here'),
+	(20,1,'Oh wow, that’s rather rude.'),
+	(21,1,'Well, maybe you will lose your shyness later.'),
+	(22,1,'In the meantime, let me give you a short introduction in who I am and what I’m actually doing here.'),
+	(23,1,'Perhaps that will make you more chatty');
 
 /*!40000 ALTER TABLE `sentence` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -95,15 +115,6 @@ UNLOCK TABLES;
 # Dump of table thema
 # ------------------------------------------------------------
 
-LOCK TABLES `thema` WRITE;
-/*!40000 ALTER TABLE `thema` DISABLE KEYS */;
-
-INSERT INTO `thema` (`id`, `sensor_type_id`, `title`, `description`)
-VALUES
-	(1,NULL,'introduction',NULL);
-
-/*!40000 ALTER TABLE `thema` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table User
@@ -114,6 +125,19 @@ UNLOCK TABLES;
 # Dump of table user_input
 # ------------------------------------------------------------
 
+LOCK TABLES `user_input` WRITE;
+/*!40000 ALTER TABLE `user_input` DISABLE KEYS */;
+
+INSERT INTO `user_input` (`id`, `title`, `type`)
+VALUES
+	(1,'Great! How about yourself','option'),
+	(2,'Please enter your name','text#name'),
+	(3,'Please suggest a name','text#name_suggestion'),
+	(4,'I prefer my friends nameless','option'),
+	(5,'That’s none of your business!','option');
+
+/*!40000 ALTER TABLE `user_input` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
