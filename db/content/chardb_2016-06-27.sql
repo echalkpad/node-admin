dcf7c0a2ec133c4b96fd3c3cf13d97f8d41b476e
Daniel Dihardja <daniel@dihardja.de>
2016-06-27 11:25:43 +0200

    data update

--- db/content/chardb_2016-06-27.sql
+++ db/content/chardb_2016-06-27.sql
@@ -7,7 +7,7 @@
 #
 # Host: 192.168.99.100 (MySQL 5.6.30-76.3)
 # Database: chardb
-# Generation Time: 2016-06-27 08:20:48 +0000
+# Generation Time: 2016-06-27 09:21:32 +0000
 # ************************************************************
 
 
@@ -74,11 +74,36 @@ UNLOCK TABLES;
 # Dump of table sentence
 # ------------------------------------------------------------
 
+LOCK TABLES `sentence` WRITE;
+/*!40000 ALTER TABLE `sentence` DISABLE KEYS */;
+
+INSERT INTO `sentence` (`id`, `mood_id`, `title`)
+VALUES
+	(1,1,'Oh, Hi there and welcome.'),
+	(2,1,'How are you doing over there?'),
+	(3,1,'You maybe wondering who I am and what I’m doing here, huh?'),
+	(4,1,'Well, to get things started, let me tell a little bit about myself.'),
+	(5,1,'First of all: I’m your Character.'),
+	(6,1,'Actually, I’m not only your Character, but “Character” is also the name people gave me.'),
+	(7,1,'Yeah, I know, it’s a strange one, people are curious about it all the time. For convenience sake, just call me “C” '),
+	(8,1,'But enough of me, how about you? With whom do I have the pleasure of speaking?');
+
+/*!40000 ALTER TABLE `sentence` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table thema
 # ------------------------------------------------------------
 
+LOCK TABLES `thema` WRITE;
+/*!40000 ALTER TABLE `thema` DISABLE KEYS */;
+
+INSERT INTO `thema` (`id`, `sensor_type_id`, `title`, `description`)
+VALUES
+	(1,NULL,'introduction',NULL);
+
+/*!40000 ALTER TABLE `thema` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table User
@@ -86,7 +111,7 @@ UNLOCK TABLES;
 
 
 
-# Dump of table user_option
+# Dump of table user_input
 # ------------------------------------------------------------
 
 
